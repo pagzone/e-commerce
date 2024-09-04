fbf740742ed142513640985788a3527002c2d202
xyugen <alex.arias.jorym@gmail.com>
2024-09-04 17:45:31 +0800

    feat: initialize prisma

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
 import { config } from '@/lib/config';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const client = new MongoClient(config.database.CONNECTION_URI, {
+const client = new MongoClient(config.database.URL, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
