2e378c31bcfc15c59c695d4e157f9dceda71efab
Kaizouku14 <mandaalv72@gmail.com>
2024-09-08 12:34:09 +0800

    Refactor: Removed redundant code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import FeaturedProducts from "@/components/FeaturedProducts";
 export default function Home() {
   return (
      <div className="relative h-96 md:mx-[75px] px-6 ">
-       <FeaturedProducts />
+    
+        <FeaturedProducts />
      </div>
   );
 }
